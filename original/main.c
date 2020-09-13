1979117d5e37f8ce9204cdc6361ab9ff08f7389f
wakasa51 <y.takashi51@gmail.com>
2020-09-13 23:00:31 +0900

    edited original

--- original/main.c
+++ original/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello, world\n");
+int main(int argc, char *argv[]) {
+  int i;
+  if (argc < 2) {
+    fprintf(stderr, "%s: file name not given\n", argv[0]);
+    exit(1);
+  }
 
   return 0;
 }
